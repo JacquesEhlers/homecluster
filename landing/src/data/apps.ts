aa9dfeaf05bc012d442961fb97afc5f49dc352a5
Jacques Ehlers <jacquesehlers15@gmail.com>
2026-01-14 15:22:17 +0100

    added public URLs to landing page

--- landing/src/data/apps.ts
+++ landing/src/data/apps.ts
@@ -10,7 +10,7 @@ export const apps: AppLink[] = [
   {
     name: "Plex",
     url: "https://plex.jacquesops.site",
-    description: "Media server",
+    description: "Plex media server",
     icon: "/icons/plex.webp",
     group: "Media",
   },
@@ -22,10 +22,38 @@ export const apps: AppLink[] = [
     group: "Media",
   },
   {
+    name: "Jellyfin",
+    url: "https://jellyfin.jacquesops.site",
+    description: "Jellyfin media server",
+    icon: "/icons/jellyfin.png",
+    group: "Media",
+  },
+  {
+    name: "Netflix",
+    url: "https://netflix.com",
+    description: "Netflix",
+    icon: "/icons/netflix.png",
+    group: "Media",
+  },
+  {
     name: "Game of Life",
     url: "https://gameoflife.jacquesops.site",
     description: "Conway's Game of Life",
     icon: "/icons/gameoflife.png",
-    group: "Media",
+    group: "Games",
+  },
+  {
+    name: "Chess",
+    url: "https://www.chess.com/home",
+    description: "chess.com",
+    icon: "/icons/chess.png",
+    group: "Games",
+  },
+  {
+    name: "Greenchoice",
+    url: "https://dynamischcontract.greenchoice.nl/verbruik?date=2026-01-14&type=costs",
+    description: "Power + Gas",
+    icon: "/icons/greenchoice.png",
+    group: "Utilities",
   },
 ];
